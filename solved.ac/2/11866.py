56b7c97523a43f25efa0878a94f1f972ad5e7f63
haru <jiaiha@naver.com>
2022-08-02 23:51:26 +0900

    백준알고리즘/solved.ac/2

--- solved.ac/2/11866.py
+++ solved.ac/2/11866.py
@@ -1,14 +1,30 @@
-# import sys
-# n, k = map(int,sys.stdin.readline().split())
-# lst_f = [False] * n
-# cnt = k-1
-# result = []
-# while lst_f != [True]*n:
-#     cnt %= n
-#     if lst_f[cnt] == False:
-#         lst_f[cnt] = True
-#         result.append(cnt+1)
-#     cnt += k
-#
-#
-# print(result)
+import sys
+n, k = map(int,sys.stdin.readline().split())
+lst_f = [False] * n
+result = []
+cnt = 0
+num = k-1
+while lst_f != [True]*n:
+    num %= n
+    if lst_f[k-1] == False:
+        lst_f[num] = True
+        result.append(num+1)
+        num += 1
+        cnt += 1
+    elif lst_f[num] == False and cnt == k:
+        lst_f[num] = True
+        result.append(num+1)
+        cnt = 1
+        num += 1
+
+    elif lst_f[num] == False:
+        cnt += 1
+        num += 1
+
+    else:
+        num += 1
+print("<",end="")
+for i in range(len(result)-1):
+    print(result[i],end=", ")
+print(result[-1],end="")
+print(">",end="")
