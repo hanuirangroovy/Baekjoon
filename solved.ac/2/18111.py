442cb1f3a1c06f08ee25215c270743755e709066
haru <jiaiha@naver.com>
2022-08-14 00:11:27 +0900

    백준알고리즘/solved.ac/2

--- solved.ac/2/18111.py
+++ solved.ac/2/18111.py
@@ -1,26 +1,23 @@
+import sys
 
-n, m, b = map(int, input().split())
+n, m, b = map(int, sys.stdin.readline().split())
 arr = []
-result_time = 99999999999999999999999999999999999
+result_time = float('inf')
 result_h = 0
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
+    arr.append(list(map(int, sys.stdin.readline().split())))
 for floor in range(257):
-
+    max_f = 0
+    min_f = 0
     for i in range(n):
-        max_f = 0
-        min_f = 0
         for j in range(m):
             if arr[i][j] >= floor:
-                min_f += floor - arr[i][j]
+                max_f += arr[i][j] - floor
             else:
-                max += arr[i][j] - floor
-        invent = max_f + b
-        if invent < min_f:
-            continue
-        time = 2* max_f + min_f
-        if time <= result_time:
-            result_time = time
+                min_f += floor - arr[i][j]
+    if max_f + b >= min_f:
+        if (2* max_f + min_f) <= result_time:
+            result_time = (2* max_f + min_f)
             result_h = floor
 print(result_time, result_h)
 
