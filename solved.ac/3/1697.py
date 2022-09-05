4a15dcf9cdd3c48cb596d73f576b6e8cf1415bee
haru <jiaiha@naver.com>
2022-09-06 00:09:54 +0900

    백준알고리즘/solved.ac/3

--- solved.ac/3/1697.py
+++ solved.ac/3/1697.py
@@ -3,16 +3,16 @@ from collections import deque
 
 def bfs(x):
     queue = deque()
-    queue.append([x])
+    queue.append(x)
     while queue:
         x = queue.popleft()
         if x == k:
             return visited[x]
         for i in (x-1, x+1, 2*x):
-            if not visited[i] and 0 <= i <= 100001:
+            if 0 <= i <= 100000 and not visited[i]: ## 순서 확인
                 visited[i] = visited[x] + 1
                 queue.append(i)
 
 n, k = map(int, sys.stdin.readline().split())
-visited = [0 for _ in range(100001)]
+visited = [0] * 100001
 print(bfs(n))
