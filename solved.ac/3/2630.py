edc3c254abe6fa228a7a39919d59262cc7382f23
haru <jiaiha@naver.com>
2022-09-20 23:46:17 +0900

    백준알고리즘/solved.ac/3

--- solved.ac/3/2630.py
+++ solved.ac/3/2630.py
@@ -1,29 +1,25 @@
 import sys
 
 def dfs(x, y, n):
-    global white, blue
-    check = arr[x][y]
     for i in range(x,x + n):
         for j in range(y, y + n):
-            if arr[i][j] != check:
-                for k in range(2):
-                    for l in range(2):
-                        dfs(x + k * n // 2, y + l * n // 2, n // 2)
+            if arr[x][y] != arr[i][j]:
+                print("(", end="")
+                dfs(x, y, n // 2)
+                dfs(x, y + n // 2, n // 2)
+                dfs(x + n // 2, y, n // 2)
+                dfs(x + n // 2, y + n // 2, n // 2)
+                print(")", end="")
                 return
-    if check == 0:
-        white += 1
+    if arr[x][y] == 1:
+        print(1, end="")
     else:
-        blue += 1
+        print(0, end="")
 
 n = int(sys.stdin.readline())
 arr = []
 
 for _ in range(n):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-
-white, blue = 0, 0
+    arr.append(list(map(int, sys.stdin.readline().strip())))
 
 dfs(0,0,n)
-
-print(white)
-print(blue)
